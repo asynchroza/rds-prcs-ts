4020520ea36de6e1cac5e500c66598629b643a73
Michael Bozhilov <michaelbozhilov@gmail.com>
2025-03-14 22:45:06 +0200

    Add compose configuration for publisher

--- publisher/publisher.py
+++ publisher/publisher.py
@@ -4,9 +4,14 @@ import time
 import uuid
 import redis
 
+from dotenv import load_dotenv
+from os import environ
+
+load_dotenv('.env.publisher')
+
 # Redis connection details (modify host and port if needed)
-redis_host = "localhost"
-redis_port = 6379
+redis_host = environ["REDIS_HOST"]
+redis_port = environ["REDIS_PORT"]
 target_duration = timedelta(minutes=1)
 batch_size = 1000
 
@@ -15,7 +20,7 @@ def publisher():
         connection = redis.Redis(host=redis_host, port=redis_port)
     except redis.ConnectionError:
         print("Error: Failed to connect to Redis server")
-    exit(1)
+        exit(1)
 
     start_time = datetime.now()
     total_messages = 0
@@ -35,10 +40,10 @@ def publisher():
             time.sleep(random.uniform(0.1, 0.5))
 
     except Exception as e:
-    print(f"Error: {e}")
+        print(f"Error: {e}")
 
     finally:
-    print(f"Total messages published: {total_messages}")
+        print(f"Total messages published: {total_messages}")
 
 if __name__ == "__main__":
-publisher()
+    publisher()
