f4bd4b6dbda9edb8e4df184fd1822161d42d9b73
Michael Bozhilov <michaelbozhilov@gmail.com>
2025-03-17 09:46:06 +0200

    Add scripts for checking reprocessed messages

---
+++ scripts/index.mjs
@@ -0,0 +1,24 @@
+import { createClient } from "redis"
+
+async function checkForReprocessedMessages() {
+    const countPerMessageId = {}
+
+    const client = createClient({ url: "redis://localhost:7234" })
+    await client.connect()
+
+    const messages = await client.xRange("messages:processed", "-", "+")
+
+    console.log(`Found ${messages.length} messages`)
+
+    for (const { message } of messages) {
+        if (!countPerMessageId[message.message_id]) {
+            countPerMessageId[message.message_id] = 1
+        } else {
+            countPerMessageId[message.message_id]++;
+        }
+    }
+
+    console.log(`Found ${Object.keys(countPerMessageId).length} unique message IDs`)
+}
+
+checkForReprocessedMessages()
